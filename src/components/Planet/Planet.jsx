79afcdcb70e91db5e78d17ad14511ea14281867b
skumar <kumasachin@gmail.com>
2025-07-13 19:16:23 +0100

    working

--- src/components/Planet/Planet.jsx
+++ src/components/Planet/Planet.jsx
@@ -11,7 +11,57 @@ const Planet = ({ name = "unKnown surface", data = null }) => {
   const [isDataValid, setDataValidation] = useState(true);
   const [terminalInput, setTerminalInput] = useState([]);
 
-  const { response } = useFetch(CONFIG.PATH.marsapi);
+  const response = {
+    "map":{
+       "x":16,
+       "y":16
+    },
+    "lostCell":{
+       "x":[
+          
+       ],
+       "y":[
+          
+       ]
+    },
+    "robots":[
+       {
+          "name":"R1",
+          "color":"green",
+          "currentPosition":"0 0 N",
+          "instructions":"FFFFFFFFFFFFFFFFFFF"
+       },
+       {
+         "name":"R2",
+         "color":"green",
+         "currentPosition":"1 11 N",
+         "instructions":"FRFFFFFFF"
+      },
+      {
+        "name":"R3",
+        "color":"blue",
+        "currentPosition":"3 1 N",
+        "instructions":"FFFFFFFFR"
+     },
+     {
+       "name":"R4",
+       "color":"blue",
+       "currentPosition":"2 3 S",
+       "instructions":"FFFFFFFFFFFFFFFFR"
+    },
+    {
+      "name":"R5",
+      "color":"blue",
+      "currentPosition":"4 7 S",
+      "instructions":"FFFFFFFFFFFFFFFFR"
+   },
+   {
+     "name":"R6",
+     "color":"blue",
+     "currentPosition":"4 7 S",
+     "instructions":"FFFFFFFFFFFFFFFFR"
+  }]
+ };
   const onClickHandler = () => {
     setExcutionStatus(true);
   }
